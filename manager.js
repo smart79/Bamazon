f730353c4e03213a962715132671f714f3a99056
Chris Martinez <smartinez79@protonmail.com>
2019-06-19 17:54:41 -0500

    Update to manager.js

---
+++ manager.js
@@ -0,0 +1,61 @@
+// required dependencies
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+// Define the MySQL connection parameters
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+
+    // Your username
+    user: 'root',
+
+    // Your password
+    password: 'Beason11!',
+    database: 'Bamazon'
+});
+function promptManagerAction() {
+    // console.log('___ENTER promptManagerAction___');
+
+    // Prompt the manager to select an option
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            message: 'Please select an option:',
+            choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
+            filter: function (val) {
+                if (val === 'View Products for Sale') {
+                    return 'sale';
+                } else if (val === 'View Low Inventory') {
+                    return 'lowInventory';
+                } else if (val === 'Add to Inventory') {
+                    return 'addInventory';
+                } else if (val === 'Add New Product') {
+                    return 'newProduct';
+                } else {
+                    // This case should be unreachable
+                    console.log('ERROR: Unsupported operation!');
+                    exit(1);
+                }
+            }
+        }
+    ]).then(function (input) {
+        // console.log('User has selected: ' + JSON.stringify(input));
+
+        // Trigger the appropriate action based on the user input
+        if (input.option === 'sale') {
+            displayInventory();
+        } else if (input.option === 'lowInventory') {
+            displayLowInventory();
+        } else if (input.option === 'addInventory') {
+            addInventory();
+        } else if (input.option === 'newProduct') {
+            createNewProduct();
+        } else {
+            // This case should be unreachable
+            console.log('ERROR: Unsupported operation!');
+            exit(1);
+        }
+    })
+}
