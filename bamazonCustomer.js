570b93870620cb428a544b4c87ad74aed0e10c3e
Chris Martinez <smartinez79@protonmail.com>
2019-07-20 22:32:29 -0500

    update to manager.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Beason11!",
+    password: "",
     database: "bamazon_db"
 });
 connection.connect(function (err) {
--- manager.js
+++ manager.js
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
     user: 'root',
 
     // Your password
-    password: 'Beason11!',
+    password: "",
     database: 'bamazon_db'
 });
 function promptManagerAction() {
